17675a8d3b97decd3c9bd4e67a1211c82901c8e9
bchelseab <cbborg@massart.edu>
2017-04-04 14:22:36 -0400

    php

---
+++ rebus/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Rebus</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300" rel="stylesheet">
+</head>
+
+<body>
+	<div class="box">
+    
+		<h1>An <i class="fa fa-apple" aria-hidden="true"></i> a <i class="fa fa-calendar-check-o" aria-hidden="true"></i> <i class="fa fa-key" aria-hidden="true"></i>ps the <i class="fa fa-user-md" aria-hidden="true"></i> a<i class="fa fa-balance-scale" aria-hidden="true"></i></h1>
+        <p>An apple a day keeps the doctor away</p>
+
+<?php
+print "ogres have layers";
+?>
+
+	</div>
+</body>
+</html>
